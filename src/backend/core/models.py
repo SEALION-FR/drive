fba80beb002d5fce036b4174d31f581237dc0493
hugo2805 <56994115+hugo2805@users.noreply.github.com>
2026-03-25 13:38:26 +0100

    fix: add download_zip permission

--- src/backend/core/models.py
+++ src/backend/core/models.py
@@ -871,6 +871,7 @@ class Item(TreeModel, BaseModel):
             "children_create": can_create_children,
             "destroy": can_destroy,
             "download": can_get,
+            "download_zip": can_get,
             "hard_delete": can_hard_delete,
             "favorite": can_get and user.is_authenticated,
             "link_configuration": is_owner_or_admin,
